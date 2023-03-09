ff64583924e56ffb7da0c6d6c990dbaeeba92cd9
AbhinavYdv <abhinavyadavdev@gmail.com>
2023-03-10 01:27:28 +0530

    tags can now be added

--- frontend/components/RTE.js
+++ frontend/components/RTE.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 import { Box } from "@mui/material";
 import 'suneditor/dist/css/suneditor.min.css'; // Import Sun Editor's CSS File
+import katex from 'katex'
+import 'katex/dist/katex.min.css'
 
 //import the component
 // const RichTextEditor = dynamic(() => import("react-rte"), { ssr: false });
@@ -13,15 +15,34 @@ const RTE = ({ onChange, ...props }) => {
   // const [value, setValue] = useState();
 
   return (
-    <Box>
+    <Box minHeight="100%" style={{ backgroundColor:"blue" }}>
         {/* <RichTextEditor value={value} onChange={handleOnChange} /> */}
         <SunEditor onChange={(e) => {
           // console.log(e)
           onChange(e)
-        }} {...props}
-        height="50ch"
-        autoFocus
-        
+          }}
+          {...props}
+          height="100%"
+          width="100%"
+          autoFocus
+          setOptions={{
+            buttonList: [['undo', 'redo'],
+            ['font', 'fontSize', 'formatBlock'],
+            ['paragraphStyle', 'blockquote'],
+            ['bold', 'underline', 'italic', 'strike', 'subscript', 'superscript'],
+            ['fontColor', 'hiliteColor', 'textStyle'],
+            ['removeFormat'],
+            ['outdent', 'indent'],
+            ['align', 'horizontalRule', 'list', 'lineHeight'],
+            ['table', 'link', 'image', 'video', 'audio', 'math'],
+            ['imageGallery'],
+            ['fullScreen', 'showBlocks', 'codeView'],
+            ['preview', 'print'],
+            ['save',],
+            ['-left', '#fix', 'dir_ltr', 'dir_rtl'],
+          ],
+            katex: katex
+          }}
         />
     </Box>
   )
--- frontend/components/richTextDialog.js
+++ frontend/components/richTextDialog.js
@@ -1,23 +1,75 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import { Autocomplete } from '@mui/material';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
+import { withStyles } from '@mui/styles';
 import DialogTitle from '@mui/material/DialogTitle';
 import RTE from './RTE';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { backend } from '@/query.config';
 
-export default function RichTextDialog({ onChange, onSubmit, onCancel, ...props }) {
+const styles = {
+  dialogPaper: {
+      minHeight: '80vh',
+      maxHeight: '80vh',
+  },
+};
+
+function RichTextDialog({ classes, onChange, onSubmit, onCancel, onTagSelect, showTagSelector, ...props }) {
+  const [ allTags, setAllTags ] = useState([])
+
+  useEffect(() => {
+      const getTags = async () => {
+        try {
+          const res = await axios.get(`${backend}/tags/get`)
+          const data = res.data.map((tag) => {
+            return {
+              name: tag.tag_name,
+              id: tag.id.toString(),
+              type: "tag"
+            }
+          })
+          setAllTags(data);
+        } catch (e) {
+          console.log(e)
+        }
+      }
+      if (allTags.length == 0)
+          getTags();
+  })
+
+
   return (
     <div>
-      <Dialog {...props}>
+      <Dialog {...props}
+        classes={{ paper: classes.dialogPaper }}
+        fullWidth
+        maxWidth="md"
+      >
         <DialogTitle >Type below</DialogTitle>
         <DialogContent>
           <RTE
             onChange={onChange}
             {...props}
           />
+          {
+            showTagSelector &&
+            <Autocomplete
+              multiple
+              getOptionLabel={(option) => (option.name)}
+              options={allTags}
+              renderInput={(params) => <TextField {...params} label="Choose tags" />}
+              onChange={(_, value) => {
+                  onTagSelect(value.map(tag => `<${tag.name}>`).join(""));
+              }}
+              sx={{ mt: 1.5 }}
+            />
+          }
         </DialogContent>
         <DialogActions>
           <Button onClick={onCancel}>Cancel</Button>
@@ -27,3 +79,5 @@ export default function RichTextDialog({ onChange, onSubmit, onCancel, ...props 
     </div>
   );
 }
+
+export default withStyles(styles)(RichTextDialog);
